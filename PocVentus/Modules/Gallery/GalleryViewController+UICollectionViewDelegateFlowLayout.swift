74ec652d1b44972f1ffd17cd0f2b64467289aabe
Júlio Garavelli <juliocgaravelli@gmail.com>
2019-09-22 01:34:57 -0300

    Adjusts on collectionViewFlowLayout.

---
+++ PocVentus/Modules/Gallery/GalleryViewController+UICollectionViewDelegateFlowLayout.swift
@@ -0,0 +1,30 @@
+//
+//  GalleryViewController+UICollectionViewDelegate.swift
+//  PocVentus
+//
+//  Created by Julio Garavelli on 21/09/19.
+//  Copyright © 2019 Julio Garavelli. All rights reserved.
+//
+
+import UIKit
+
+//MARK: UICollectionViewDelegate
+extension GalleryViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize = (collectionView.bounds.width - 48) / 3
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+}
